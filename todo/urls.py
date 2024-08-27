6122b8f115dbbec50c24d2f267b98edb2b249d92
Michael Chary <michaelchary081@gmail.com>
2024-08-27 12:10:45 +0330

    s10

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     path('todos/', views.all_todos),
-    path('todos/<int:todo_id>/', views.todo_deatil_view)
+    path('todos/<int:todo_id>/', views.todo_deatil_view),
+    path('cbv/', views.ListTodoView.as_view()),
+    path('cbv/<int:todo_id>/', views.DetailView.as_view()),
+    path('todos/mixins/', views.TodosListMixinApiView.as_view()),
+    path('todos/mixins/<int:pk>', views.TodoDetailMixinApiView.as_view()),
+    path('todos/generics/', views.TodoGenericApiView.as_view()),
+    path('todos/generics/<int:pk>', views.TodoGenericDetailView.as_view())
+    # path('cbv/',views.ManageTodoApiView.as_view()),
 ]
